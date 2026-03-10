1a7930e5097f0f8e1f96a07a6e002cc356840a9c
loic <33072306+slayer17@users.noreply.github.com>
2026-03-10 08:37:14 +0100

    Update spacelab.py

--- spacelab.py
+++ spacelab.py
@@ -56,7 +56,7 @@ def upload():
 
     kernel = np.ones((9,9),np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=2)
-
+mask = cv2.dilate(mask, kernel, iterations=1)
     contours,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Diagnostic : dessiner tous les blobs
