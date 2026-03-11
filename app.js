a9b59769d21b8fe39e47c2d0467798395f727508
loic <33072306+slayer17@users.noreply.github.com>
2026-03-11 07:55:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const cardName = document.getElementById("cardName");
 
 let currentStream = null;
 
+   
 /* =====================================================
    CAMERA
 ===================================================== */
