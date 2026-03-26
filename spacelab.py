91773daa70d2a72733333b94f9c6bd45314c5ee9
loic <33072306+slayer17@users.noreply.github.com>
2026-03-26 12:09:26 +0100

    Update spacelab.py

--- spacelab.py
+++ spacelab.py
@@ -1048,6 +1048,7 @@ def main():
         "result": result,
     }
 
+    
     with open(out_dir / "result.json", "w", encoding="utf-8") as f:
         json.dump(json_result, f, indent=2, ensure_ascii=False)
 
